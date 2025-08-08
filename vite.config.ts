3516184d68ff689bb0b9474076a22a1f2456806b
nadeem3213 <nadeemsk351@gmail.com>
2025-08-08 03:42:45 -0700

    Adding link to pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
+  base: "/Team_project_PICT/", // 
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    
   ].filter(Boolean),
   resolve: {
     alias: {
