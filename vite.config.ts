4e0092c5f9ea9811a4d01accedf5068ee29b3aae
nadeem3213 <nadeemsk351@gmail.com>
2025-08-08 05:23:05 -0700

    fixed package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
-  base: "/Team_project_PICT/", // 
+export default defineConfig(() => ({ 
   server: {
     host: "::",
     port: 8080,
